e3f51300459acd8f80663507d29bc43780f7dd5f
Yeattt <nekz.8896@gmail.com>
2024-05-06 20:04:52 -0500

    Basic upload functionality finished

--- src/app/uploader/components/upload-field/upload-field.component.ts
+++ src/app/uploader/components/upload-field/upload-field.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import { UploaderService } from '../../services/uploader.service';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'uploader-upload-field',
@@ -6,5 +9,44 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class UploadFieldComponent {
+  @ViewChild('fileUploadInput', { static: false })
+  public fileUploadInput!: ElementRef<HTMLInputElement>;
 
+  public file?: File;
+
+  constructor(
+    private uploaderService: UploaderService,
+  ) { }
+
+  public onDragImage(event: DragEvent) {
+    event.preventDefault();
+  }
+
+  public onDropImage(event: DragEvent) {
+    event.preventDefault();
+
+    if (event.dataTransfer) {
+      if (this.fileUploadInput) {
+        const files: FileList = event.dataTransfer.files;
+
+        this.fileUploadInput.nativeElement.files = files;
+        this.file = files[0];
+
+        this.uploaderService.uploadImage(this.file)
+          .subscribe(resp => {
+            console.log(resp);
+          });
+      };
+    };
+  }
+
+  public onAttachImage(event: any) {
+    event.preventDefault();
+
+    if (event.target.files) {
+      this.file = event.target.files[0];
+    }
+
+    console.log(this.file);
+  }
 }
--- src/app/uploader/services/uploader.service.ts
+++ src/app/uploader/services/uploader.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Observable, catchError } from 'rxjs';
+import { Image } from '../interfaces';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UploaderService {
-  
+  private readonly apiUrl: string = 'http://localhost:3000/api';
+  public isLoading: boolean = true;
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  public uploadImage(image: File): Observable<Image> {
+    const url: string = `${this.apiUrl}/images/upload`;
+
+    const formData: FormData = new FormData();
+    formData.append('image', image);
+
+    this.isLoading = false;
+
+    return this.http.post<Image>(url, formData);
+
+  }
 }
