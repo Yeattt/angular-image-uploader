f0ffe7d5ef98f7414ced3e5134e25280cab459f3
Yeattt <nekz.8896@gmail.com>
2024-05-06 23:29:34 -0500

    Image card component added

---
+++ src/app/uploader/components/image-card/image-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'uploader-image-card',
+  templateUrl: './image-card.component.html',
+  styles: ``
+})
+export class ImageCardComponent {
+  
+}
