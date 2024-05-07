9f2c0b1e4a33a850a26941d44d475abfe3de8566
Yeattt <nekz.8896@gmail.com>
2024-05-06 23:29:47 -0500

    Uploaded image page added

---
+++ src/app/uploader/pages/uploaded-image-page/uploaded-image-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'uploader-uploaded-image-page',
+  templateUrl: './uploaded-image-page.component.html',
+  styles: ``
+})
+export class UploadedImagePageComponent {
+
+}
